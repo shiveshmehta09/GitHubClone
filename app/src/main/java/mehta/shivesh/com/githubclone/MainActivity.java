93f6253e78e39f616e55136d2caa9b2d49730fc1
Shivesh <shiveshmehta09@gmail.com>
2018-08-02 12:56:59 +0530

    Project Created

---
+++ app/src/main/java/mehta/shivesh/com/githubclone/MainActivity.java
@@ -0,0 +1,13 @@
+package mehta.shivesh.com.githubclone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
